de371192e8dc0ffc33a3eec179aff327c50f92c8
GestionIzia <gestion-izia@cfa-afia.fr>
2024-03-28 11:56:25 +0100

    Add modal by cards + card selected color

--- react-app-aspi-frontend/src/App.jsx
+++ react-app-aspi-frontend/src/App.jsx
@@ -1,52 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
-import Card from './components/Card.jsx';
-import Modal from './components/Modal.jsx'; 
+import Card from './components/Card';
+import Modal from './components/Modal';
 
 function App() {
-  const [show, setShow] = useState(false);
-  const [cardInfos, setCardInfos] = useState([]); 
-  const [htmlContent, setHtmlContent] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [cardInfos, setCardInfos] = useState([]);
+  const [selectedOfferIndex, setSelectedOfferIndex] = useState(null); // Nouvel état pour stocker l'index de la carte sélectionnée
+  const API_BASE_URL = 'http://localhost:5244/Scraping';
 
-  const openModal = async (urlOffer) => {
-    // Appel de l'API pour récupérer le contenu HTML de l'URL spécifique
-    try {
-      const response = await axios.get(`http://localhost:5244/Scraping/scrape-html-content?url=${encodeURIComponent(urlOffer)}`);
-      setHtmlContent(response.data);
-      setShow(true);
-    } catch (error) {
-      console.error('Une erreur s\'est produite lors de la récupération du contenu HTML :', error);
-    }
+  const openModal = (index) => { // Prend en argument l'index de la carte sélectionnée
+    setSelectedOfferIndex(index); // Met à jour l'index de la carte sélectionnée
+    setShowModal(true);
   };
+
   const closeModal = () => {
-    setShow(false);
+    setShowModal(false);
   };
 
   useEffect(() => {
-    axios.get('http://localhost:5244/Scraping/scrape-job-offers')
-      .then(response => {
+    const fetchJobOffers = async () => {
+      try {
+        const response = await axios.get(`${API_BASE_URL}/scrape-html-content`);
         if (response.data && response.data.length > 0) {
-          setCardInfos(response.data.slice(0, 12)); // Prendre les douze premières cartes
-        
+          setCardInfos(response.data.slice(0, 12));
         }
-      })
-      .catch(error => {
-        console.error('Une erreur s\'est produite lors de la récupération des données :', error);
-      });
+      } catch (error) {
+        console.error('Error fetching job offers:', error);
+      }
+    };
+    fetchJobOffers();
   }, []);
 
   return (
-    <div className={`App ${show ? 'modal-open' : ''}`}>
+    <div className={`App ${showModal ? 'modal-open' : ''}`}>
       <div className="card-grid">
         {cardInfos.map((cardInfo, index) => (
-          <Card key={index} cardInfo={cardInfo} openModal={openModal} />
+          <Card
+            key={index}
+            cardInfo={cardInfo}
+            openModal={() => openModal(index)} // Passe l'index de la carte lorsqu'elle est sélectionnée
+            isSelected={selectedOfferIndex === index} // Ajoute une prop isSelected avec une valeur booléenne
+          />
         ))}
       </div>
-      
-      <Modal show={show} closeModal={closeModal} htmlContent={htmlContent} />
+      {showModal && (
+        <Modal show={showModal} closeModal={closeModal} cardInfo={cardInfos[selectedOfferIndex]} />
+      )}
     </div>
   );
 }
